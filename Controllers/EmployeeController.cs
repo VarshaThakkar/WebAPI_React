dabaae79edda3c91104cd5779d57ad00690f4f68
Varsha Thakkar <96332770+VarshaThakkar@users.noreply.github.com>
2022-03-11 21:11:11 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebAPI_React.Models;
+
+namespace WebAPI_React.Controllers
+{
+    [EnableCors("*","*","*")]
+    public class EmployeeController : ApiController
+    {
+        public List<Employee> GetAll()
+        {
+            List<Employee> empList = new List<Employee>
+            {
+                new Employee{ Id=101,Name="varsha",Salary=10000},
+                new Employee{ Id=102,Name="Krishav",Salary=20000},
+                new Employee{ Id=103,Name="Mahesh",Salary=30000}
+            };
+            return empList;
+        }
+    }
+}
